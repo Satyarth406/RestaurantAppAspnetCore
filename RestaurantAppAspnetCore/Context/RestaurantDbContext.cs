e5500c9b4d7411c1c74649520e2352670c79808e
Satyarth Misra <misrasatyarth@outlook.com>
2020-09-08 00:12:58 +0530

    Add project files.

---
+++ RestaurantAppAspnetCore/Context/RestaurantDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EFcoreImplementation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFcoreImplementation.Context
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options):
+            base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ RestaurantAppAspnetCore/Repository/RestaurantRepository.cs
@@ -0,0 +1,46 @@
+﻿using EFcoreImplementation.Context;
+using EFcoreImplementation.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFcoreImplementation.Repository
+{
+    public class RestaurantRepository : IRestaurantRepository
+    {
+        private readonly RestaurantDbContext _restaurantDbContext;
+
+        public RestaurantRepository(RestaurantDbContext restaurantDbContext)
+        {
+            _restaurantDbContext = restaurantDbContext;
+        }
+        public List<Restaurant> AllRestaurants()
+        {
+            return _restaurantDbContext.Restaurants.ToList();
+        }
+
+        public Restaurant CreateRestaurant(Restaurant newRestaurant)
+        {
+            _restaurantDbContext.Restaurants.Add(newRestaurant);
+            _restaurantDbContext.SaveChanges();
+            return newRestaurant;
+        }
+
+        public void DeleteRestaurant(int id)
+        {
+            _restaurantDbContext.Restaurants.Remove(_restaurantDbContext.Restaurants.Find(id));
+            _restaurantDbContext.SaveChanges();
+        }
+
+        public Restaurant GetRestaurantById(int id)
+        {
+            return _restaurantDbContext.Restaurants.FirstOrDefault(t=>t.Id==id);
+        }
+
+        public Restaurant UpdateRestaurant(Restaurant updatedRestaurant)
+        {
+            _restaurantDbContext.Restaurants.Update(updatedRestaurant);
+            _restaurantDbContext.SaveChanges();
+            return updatedRestaurant;
+        }
+    }
+}
